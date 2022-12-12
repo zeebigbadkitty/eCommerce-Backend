2ad11bcc2c49ca6f3cde365feeef8f1346bc09d3
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-11 23:04:45 -0600

    Product tag model started.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+      
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //References the Product model's id. 
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //References the tag model's id. 
+    },
+    
   },
   {
     sequelize,
