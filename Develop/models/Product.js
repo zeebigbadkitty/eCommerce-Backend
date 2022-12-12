8f7084ff5f8a605493e9a6bb3b6e5d969703d494
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-11 23:02:23 -0600

    Working on product model.

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,33 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+      
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+      //validates that the value is a decimal. 
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+      //Default value of 10, Validates that the value if numeric. 
+      
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //References the Category Model's Id
+      
+    },
   },
   {
     sequelize,
