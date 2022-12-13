216fe0a966b9e971b810f2d8f77cccd19a4007f3
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-12 18:13:54 -0600

    Foreign Keys and belongs to is getting hashed out.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey:
+})
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag,{
+  foreignKey:
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product,{
+  foreignKey:
+})
+
 
 module.exports = {
   Product,
