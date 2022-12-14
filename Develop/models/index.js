47e3286be2ef4210651154529f51c428d1b93188
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-13 18:08:38 -0600

    Okay, finally got the through model correct.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -18,7 +18,7 @@ Product.belongsToMany(Tag,{
     model: ProductTag,
     unique: false},
     as: 'product_tags'
-  foreignKey: "product_id"
+
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
@@ -26,7 +26,7 @@ Tag.belongsToMany(Product,{
     model: ProductTag,
     unique: false},
     as: 'tag_products'
-  foreignKey: "tag_id"
+
 })
 
 
